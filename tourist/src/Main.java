bb106b460158fd155f311aab0eb81fcecf1fc9f1
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-11-20 21:12:55 +0600

    added crud

--- tourist/src/Main.java
+++ tourist/src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello world!");
-        System.out.println("Almash Palmash");
         Address address1 = new Address("Almaty","Alatau","Abay",50,5);
         User user1 = new User(address1,160902,"Almash","Yerzhanova","yerzhanova@gmail.com",8705777);
         User user2 = new User(address1,1505902,"Almash","Yerzhanova","yerzhanova@gmail.com",8705777);
@@ -14,9 +13,7 @@ public class Main {
         RecordManagement r = new RecordManagement();
         r.add(user2);
         r.display();
-        r.display();
-        r.delete(1505902);
-        r.delete(16090452);
+        r.update(1505902,input);
         r.display();
     }
 }
