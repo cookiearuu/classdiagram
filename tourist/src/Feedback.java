499dffa869b7e633b2594d29467bffd62f3986c2
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-11-20 18:34:39 +0600

    continue

--- tourist/src/Feedback.java
+++ tourist/src/Feedback.java
@@ -1,2 +1,35 @@
 public class Feedback {
+    public String comment;
+    public User user;
+    public Tour tourID;
+
+    public Feedback(String comment,User user , Tour tourID) {
+        this.comment = comment;
+        this.tourID=tourID;
+        this.user = user;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Tour getTourID() {
+        return tourID;
+    }
+
+    public void setTourID(Tour tourID) {
+        this.tourID = tourID;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
