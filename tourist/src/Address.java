84885b0e01c5fc55b5ddaaf28a1d9150bab1a54f
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-11-20 15:03:32 +0600

    added addres class

---
+++ tourist/src/Address.java
@@ -0,0 +1,70 @@
+public class Address {
+    public String city;
+    public String district;
+    public String street;
+    public int house;
+    public char flat;
+
+    public Address() {
+        super();
+    }
+
+    public Address(String city,String district , String street , int house , char flat) {
+        this.city = city;
+        this.district= district;
+        this.street = street;
+        this.house = house ;
+        this.flat=flat;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public char getFlat() {
+        return flat;
+    }
+
+    public void setFlat(char flat) {
+        this.flat = flat;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", district='" + district + '\'' +
+                ", street='" + street + '\'' +
+                ", house=" + house +
+                ", flat=" + flat +
+                '}';
+    }
+}
---
+++ tourist/src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    private int userID;
+    private String name ;
+    private String last_name ;
+    private String email;
+    private int phone_number;
+    private Address address;
+
+
+}
