6623da30b6ddda538c1b696f423046707d7dcea2
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-11-20 18:38:55 +0600

    continue

--- tourist/src/Cards.java
+++ tourist/src/Cards.java
@@ -1,2 +1,21 @@
+import java.util.Date;
+
 public class Cards {
+    public  User userID;
+    public String type;
+    public int cardNumber;
+    public int CVV;
+    public Date date;
+
+    public Cards() {
+    }
+
+    public Cards(int cardNumber,int CVV,
+                 User userID, Date date , String type) {
+        this.cardNumber = cardNumber;
+        this.CVV=CVV;
+        this.userID=userID;
+        this.date = date;
+        this.type=type;
+    }
 }
